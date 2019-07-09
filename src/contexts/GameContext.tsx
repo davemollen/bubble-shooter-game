08847a441eea9cc12b5a5a1b878b4a6649e6b1bc
davemollen <davemollen@gmail.com>
2019-07-09 12:39:36 +0200

    type check game state

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -1,40 +1,30 @@
 import React, { createContext, useState, useEffect } from 'react'
+import { GameState } from '../types/GameTypes'
 
 export const GameContext = createContext({})
 
 const GameContextProvider: React.FC = (props) => {
-  const [state, setState] = useState({
-    gameTable: [
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-      [{color: ''}],
-    ],
+  const [state]: GameState = useState({
+    gameTable: [[],[],[],[],[],[],[],[],[],[],[]],
     shootingBubble: {color: 'red'}
   })
 
   useEffect(() => {
-    for(let column=8; column>=0; column--){
+    for(let column=10; column>=0; column--){
 
       for(let row=8; row>=0; row--){
-        if(row < 6){
+        if(row < 3){
           const allColors = ['blue', 'red', 'purple', 'green'];
           const randomColor = allColors[Math.floor(Math.random() * 4)];
           state.gameTable[row][column] = { color: randomColor }
         } else {
-          state.gameTable[row][column] = { color: '' }
+          state.gameTable[row][column] = { color: null }
         }
       }
 
     }
-  }, [])
+  }, [state.gameTable])
 
-  console.log('gameTable', state.gameTable)
   return (
     <GameContext.Provider value={ {...state} }>
       {props.children}
---
+++ src/types/GameTypes.tsx
@@ -0,0 +1,10 @@
+interface Bubble {
+  color: string | null
+}
+
+interface Bubbles {
+  gameTable: Bubble[][],
+  shootingBubble: Bubble
+}
+
+export interface GameState extends Array<Bubbles|Function>{}
